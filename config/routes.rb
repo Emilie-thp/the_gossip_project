47e82060b66aef07babc8055801945c195ffa589
Emilie-thp <emilie.marjollet@orange.fr>
2019-10-28 20:22:02 +0100

    fix welcome

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 	get 'gossip/:id', to: 'dynamic_pages#gossip', as: 'gossip'
 
   get '/welcome/:id', to: 'dynamic_pages#welcome', as: 'welcome'
-  
+  get '/welcome', to: 'dynamic_pages#welcome'
+
   get '/contact', to: 'static_pages#contact'
   
   get '/team', to: 'static_pages#team'
